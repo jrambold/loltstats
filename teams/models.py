8010b81dc76b63e00ccf02aec25b85489702ce11
jrambold <jrambold@gmail.com>
2018-06-03 14:06:51 -0600

    redid models

--- teams/models.py
+++ teams/models.py
@@ -3,13 +3,37 @@ from django.db import models
 class Team(models.Model):
 	name = models.CharField(max_length=200)
 	server = models.IntegerField(default=0)
-	
+
 	def __str__(self):
 		return self.name
-	
+
 class Player(models.Model):
-	username = models.CharField(max_length=200)
+	riot_id = models.IntegerField()
+	account_id = models.IntegerField()
+	name = models.CharField(max_length=200)
+	profile_icon_id = models.IntegerField()
+	revision_date = models.IntegerField()
+	summoner_level = models.IntegerField()
 	team = models.ForeignKey(Team, on_delete=models.CASCADE)
-	
+
 	def __str__(self):
 		return self.username
+
+class FlexMatch(models.Model):
+	player = models.ForeignKey(Player, on_delete=models.CASCADE)
+	gameId = models.IntegerField()
+	champion = models.IntegerField()
+	season = models.IntegerField()
+	timestamp = models.IntegerField()
+	role = models.CharField(max_length=200)
+	lane = models.CharField(max_length=200)
+	game_version = models.CharField(max_length=200)
+	win = models.CharField(max_length=200)
+	top = models.IntegerField()
+	mid = models.IntegerField()
+	jun = models.IntegerField()
+	adc = models.IntegerField()
+	sup = models.IntegerField()
+
+	def __str__(self):
+		return self.win
--- teams/views.py
+++ teams/views.py
@@ -2,12 +2,23 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, JsonResponse
 from teams.models import Team, Player
 from django.core import serializers
+import requests
+
 
 
 def index(request):
-    return HttpResponse('team index here')
+    headers = { 'X-Riot-Token': 'RGAPI-e53f0d2f-6e1c-4b33-aac0-8d1214ffdb77' }
+    # r = requests.get('https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/wraithcube', headers=headers)
+    # r = requests.get('https://na1.api.riotgames.com/lol/match/v3/matchlists/by-account/208507406?queue=440&season=11', headers=headers)
+    r = requests.get('https://na1.api.riotgames.com/lol/match/v3/matches/2789859686', headers=headers)
+    user = r.json()
+    return JsonResponse(user)
 
-	
+
+
 def list(request):
 	data = serializers.serialize("json", Team.objects.all())
 	return JsonResponse(data, safe=False)
+
+def add_member(request):
+    return HttpResponse('this should add a team member')
