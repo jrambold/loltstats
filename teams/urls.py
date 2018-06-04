ee87924d025499180d3a475304e466764deb2f55
Jake Rambold <33973572+jrambold@users.noreply.github.com>
2018-06-04 01:39:22 -0600

    shows best win percentage by position

--- teams/urls.py
+++ teams/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
 	path('list', views.list, name='list'),
 	path('add_member', views.add_member, name='add_member'),
 	path('populate', views.populate, name='populate'),
+	path('best_by_position', views.best_by_position, name='best_by_position'),
 ]
--- teams/views.py
+++ teams/views.py
@@ -6,25 +6,21 @@ import requests
 import json
 import time
 
-
+def headers():
+	return { 'X-Riot-Token': 'RGAPI-b0447c72-3b71-4256-9063-fe4fc5ab2479' }
 
 def index(request):
-    headers = { 'X-Riot-Token': 'RGAPI-b0447c72-3b71-4256-9063-fe4fc5ab2479' }
-    # r = requests.get('https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/wraithcube', headers=headers)
-    # r = requests.get('https://na1.api.riotgames.com/lol/match/v3/matchlists/by-account/208507406?queue=440&season=11', headers=headers)
-    r = requests.get('https://na1.api.riotgames.com/lol/match/v3/matches/2789859686', headers=headers)
-    user = r.json()
-    return JsonResponse(user)
+    return JsonResponse({'Hello': 'World'})
 
 
-
+#lists all teams
 def list(request):
 	data = serializers.serialize("json", Team.objects.all())
 	r = json.loads(data)
 	return JsonResponse(r, safe=False)
-	
+
+#takes headers team
 def populate(request):
-	headers = { 'X-Riot-Token': 'RGAPI-b0447c72-3b71-4256-9063-fe4fc5ab2479' }
 	inHeaders = request.META
 	myteam = Team.objects.get(name = inHeaders['HTTP_TEAM'])
 	players = myteam.player_set.all()
@@ -40,7 +36,7 @@ def populate(request):
 			most_recent = 0
 		
 		while more_matches:
-			r = requests.get(f"https://na1.api.riotgames.com/lol/match/v3/matchlists/by-account/{player.account_id}?queue=440&season=11&beginIndex={index}", headers=headers).json()
+			r = requests.get(f"https://na1.api.riotgames.com/lol/match/v3/matchlists/by-account/{player.account_id}?queue=440&season=11&beginIndex={index}", headers=headers()).json()
 			if len(r['matches']) == 0:
 				more_matches = False
 			for match in r['matches']:
@@ -54,7 +50,7 @@ def populate(request):
 				
 				if timestamp > most_recent:
 					
-					details = requests.get(f"https://na1.api.riotgames.com/lol/match/v3/matches/{gameId}", headers=headers).json()
+					details = requests.get(f"https://na1.api.riotgames.com/lol/match/v3/matches/{gameId}", headers=headers()).json()
 					time.sleep(2)
 					
 					game_version = details['gameVersion']
@@ -115,13 +111,13 @@ def populate(request):
 
 	return JsonResponse({'Games_Created': count})
 
+#takes headers team and name
 def add_member(request):
-	headers = { 'X-Riot-Token': 'RGAPI-b0447c72-3b71-4256-9063-fe4fc5ab2479' }
 	inHeaders = request.META
 	name = inHeaders['HTTP_NAME']
 	myteam = Team.objects.get(name = inHeaders['HTTP_TEAM'])
 	
-	r = requests.get(f"https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/{name}", headers=headers).json()
+	r = requests.get(f"https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/{name}", headers=headers()).json()
 
 	player = Player(riot_id = r['id'],
 					account_id = r['accountId'],
@@ -144,5 +140,86 @@ def add_member(request):
 	
 	return JsonResponse(created)
 	
+#takes headers team and min
+def best_by_position(request):
+	inHeaders = request.META
+	if 'HTTP_MIN' in set(inHeaders):
+		min_games = int(inHeaders['HTTP_MIN'])
+	else:
+		min_games = 0
 	
+	myteam = Team.objects.get(name = inHeaders['HTTP_TEAM'])
+	
+	player = myteam.player_set.all()[1]
+	
+	top_total = 0
+	top_wins = 0
+	top_player = 'None'
 
+	mid_total = 0
+	mid_wins = 0
+	mid_player = 'None'
+
+	jun_total = 0
+	jun_wins = 0
+	jun_player = 'None'
+
+	adc_total = 0
+	adc_wins = 0
+	adc_player = 'None'
+
+	sup_total = 0
+	sup_wins = 0
+	sup_player = 'None'
+	
+	players = myteam.player_set.all()
+	
+	for player in players:
+		current_top_wins = player.flexmatch_set.filter(top=player.account_id).filter(win=True).count()
+		current_top_total = player.flexmatch_set.filter(top=player.account_id).count()
+		if percent(top_wins, top_total) < percent(current_top_wins, current_top_total) and current_top_total >= min_games:
+			top_wins = current_top_wins
+			top_total = current_top_total
+			top_player = player.name
+
+		current_mid_wins = player.flexmatch_set.filter(mid=player.account_id).filter(win=True).count()
+		current_mid_total = player.flexmatch_set.filter(mid=player.account_id).count()
+		if percent(mid_wins, mid_total) < percent(current_mid_wins, current_mid_total) and current_mid_total >= min_games:
+			mid_wins = current_mid_wins
+			mid_total = current_mid_total
+			mid_player = player.name
+			
+		current_jun_wins = player.flexmatch_set.filter(jun=player.account_id).filter(win=True).count()
+		current_jun_total = player.flexmatch_set.filter(jun=player.account_id).count()
+		if percent(jun_wins, jun_total) < percent(current_jun_wins, current_jun_total) and current_jun_total >= min_games:
+			jun_wins = current_jun_wins
+			jun_total = current_jun_total
+			jun_player = player.name
+			
+		current_adc_wins = player.flexmatch_set.filter(adc=player.account_id).filter(win=True).count()
+		current_adc_total = player.flexmatch_set.filter(adc=player.account_id).count()
+		if percent(adc_wins, adc_total) < percent(current_adc_wins, current_adc_total) and current_adc_total >= min_games:
+			adc_wins = current_adc_wins
+			adc_total = current_adc_total
+			adc_player = player.name
+			
+		current_sup_wins = player.flexmatch_set.filter(sup=player.account_id).filter(win=True).count()
+		current_sup_total = player.flexmatch_set.filter(sup=player.account_id).count()
+		if percent(sup_wins, sup_total) < percent(current_sup_wins, current_sup_total) and current_sup_total >= min_games:
+			sup_wins = current_sup_wins
+			sup_total = current_sup_total
+			sup_player = player.name
+	
+	
+	best_group = {'top': {'name': top_player, 'percent': percent(top_wins, top_total), 'wins': top_wins, 'losses': (top_total-top_wins), 'total': top_total},
+				'mid': {'name': mid_player, 'percent': percent(mid_wins, mid_total), 'wins': mid_wins, 'losses': (mid_total-mid_wins), 'total': mid_total},
+				'jun': {'name': jun_player, 'percent': percent(jun_wins, jun_total), 'wins': jun_wins, 'losses': (jun_total-jun_wins), 'total': jun_total},
+				'adc': {'name': adc_player, 'percent': percent(adc_wins, adc_total), 'wins': adc_wins, 'losses': (adc_total-adc_wins), 'total': adc_total},
+				'sup': {'name': sup_player, 'percent': percent(sup_wins, sup_total), 'wins': sup_wins, 'losses': (sup_total-sup_wins), 'total': sup_total}}
+				
+	return JsonResponse(best_group)
+
+def percent(portion, full):
+	if full == 0:
+		return 0
+	return portion/full*100
