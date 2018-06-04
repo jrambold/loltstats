e8741cb26b2da6d928e83393dd3b086fc980b95c
Jake Rambold <33973572+jrambold@users.noreply.github.com>
2018-06-03 23:39:19 -0600

    Only pulls new games

--- teams/views.py
+++ teams/views.py
@@ -33,11 +33,18 @@ def populate(request):
 	for player in players:
 		more_matches = True
 		index = 0
+		most_recent = player.flexmatch_set.order_by('-timestamp')
+		if len(most_recent) > 0:
+			most_recent = most_recent[0].timestamp
+		else:
+			most_recent = 0
+		
 		while more_matches:
 			r = requests.get(f"https://na1.api.riotgames.com/lol/match/v3/matchlists/by-account/{player.account_id}?queue=440&season=11&beginIndex={index}", headers=headers).json()
 			if len(r['matches']) == 0:
 				more_matches = False
 			for match in r['matches']:
+				
 				gameId = match['gameId']
 				champion = match['champion']
 				season = match['season']
@@ -45,64 +52,65 @@ def populate(request):
 				role = match['role']
 				lane = match['lane']
 				
-				
-				details = requests.get(f"https://na1.api.riotgames.com/lol/match/v3/matches/{gameId}", headers=headers).json()
-				time.sleep(1.5)
-				
-				game_version = details['gameVersion']
-				
-				participantID = 0
-				
-				for participant in details['participantIdentities']:
-					if (participant['player']['accountId'] == player.account_id):
-						participantID = participant['participantId']
+				if timestamp > most_recent:
 					
-				win = details['participants'][participantID-1]['stats']['win']
-				
-				if participantID > 5:
-					i = 5
-				else:
-					i = 0
+					details = requests.get(f"https://na1.api.riotgames.com/lol/match/v3/matches/{gameId}", headers=headers).json()
+					time.sleep(2)
 					
-				asdf = i
-				
-				top = 0
-				mid = 0
-				jun = 0
-				adc = 0
-				sup = 0
-				
-				for j in range(5):
-					p_role = details['participants'][j+i]['timeline']['role']
-					p_lane = details['participants'][j+i]['timeline']['lane']
-					if p_role == 'SOLO' and p_lane == 'TOP':
-						top = details['participantIdentities'][j+i]['player']['accountId']
-					elif p_role == 'SOLO' and p_lane == 'MIDDLE':
-						mid = details['participantIdentities'][j+i]['player']['accountId']
-					elif p_role == 'NONE' and p_lane == 'JUNGLE':
-						jun = details['participantIdentities'][j+i]['player']['accountId']
-					elif p_role == 'DUO_CARRY' and p_lane == 'BOTTOM':
-						adc = details['participantIdentities'][j+i]['player']['accountId']
-					elif p_role == 'DUO_SUPPORT' and p_lane == 'BOTTOM':
-						sup = details['participantIdentities'][j+i]['player']['accountId']
+					game_version = details['gameVersion']
 					
-				flex = FlexMatch(player = player,
-								gameId = gameId,
-								champion = champion,
-								season = season,
-								timestamp = timestamp,
-								role = role,
-								lane = lane,
-								game_version = game_version,
-								win = win,
-								top = top,
-								mid = mid,
-								jun = jun,
-								adc = adc,
-								sup = sup)
-				flex.save()
+					participantID = 0
+					
+					for participant in details['participantIdentities']:
+						if (participant['player']['accountId'] == player.account_id):
+							participantID = participant['participantId']
+						
+					win = details['participants'][participantID-1]['stats']['win']
+					
+					if participantID > 5:
+						i = 5
+					else:
+						i = 0
+					
+					top = 0
+					mid = 0
+					jun = 0
+					adc = 0
+					sup = 0
+					
+					for j in range(5):
+						p_role = details['participants'][j+i]['timeline']['role']
+						p_lane = details['participants'][j+i]['timeline']['lane']
+						if p_role == 'SOLO' and p_lane == 'TOP':
+							top = details['participantIdentities'][j+i]['player']['accountId']
+						elif p_role == 'SOLO' and p_lane == 'MIDDLE':
+							mid = details['participantIdentities'][j+i]['player']['accountId']
+						elif p_role == 'NONE' and p_lane == 'JUNGLE':
+							jun = details['participantIdentities'][j+i]['player']['accountId']
+						elif p_role == 'DUO_CARRY' and p_lane == 'BOTTOM':
+							adc = details['participantIdentities'][j+i]['player']['accountId']
+						elif p_role == 'DUO_SUPPORT' and p_lane == 'BOTTOM':
+							sup = details['participantIdentities'][j+i]['player']['accountId']
+						
+					flex = FlexMatch(player = player,
+									gameId = gameId,
+									champion = champion,
+									season = season,
+									timestamp = timestamp,
+									role = role,
+									lane = lane,
+									game_version = game_version,
+									win = win,
+									top = top,
+									mid = mid,
+									jun = jun,
+									adc = adc,
+									sup = sup)
+					flex.save()
 
-				count += 1
+					count += 1
+				else:
+					more_matches = False
 			index += 100
 
 	return JsonResponse({'Games_Created': count})
