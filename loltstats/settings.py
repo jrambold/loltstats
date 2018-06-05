9a77987f8dc72eaab2beea6616e668d566623fa4
jrambold <jrambold@gmail.com>
2018-06-05 15:01:19 -0600

    secret key hidden

--- loltstats/settings.py
+++ loltstats/settings.py
@@ -23,7 +23,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ywaxj%4g01kvm*iexhrcsw!z9=&+aa+pm#is6)^+g-=w%ekje5'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
