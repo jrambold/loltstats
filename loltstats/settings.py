3f5780af594b0e12309bf3d79bb2ccf5511fcd74
jrambold <jrambold@gmail.com>
2018-06-05 14:44:25 -0600

    a

--- loltstats/settings.py
+++ loltstats/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import django_heroku
-from .base import
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
